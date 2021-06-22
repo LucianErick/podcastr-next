098c5fb5fcc91e37899254b6f74be3294c86faff
Luciano Figueiredo <luciano.erick.filho@ccc.ufcg.edu.br>
2021-06-21 21:48:04 -0300

    Configurando API e desenvolvendo componentes Player e Header.

---
+++ src/pages/index.tsx
@@ -0,0 +1,47 @@
+import { useEffect } from "react";
+// SPA
+  // useEffect(() => {
+  //   fetch("http://localhost:3333/episodes").then(response => response.json()).then(data => console.log(data));
+  // }, []);
+
+// SSR
+
+// SSG
+
+export default function Home(props) {
+  useEffect(() => {
+    fetch("http://localhost:3333/episodes").then(response => response.json()).then(data => console.log(data));
+  }, []);
+  return (
+    <>
+      <p>{JSON.stringify(props.episodes)}</p>
+    </>
+  );
+}
+
+
+export async function getStaticProps() {
+    const response = await fetch("http://localhost:3333/episodes");
+    const data = await response.json();
+
+    return { 
+      props: {
+        episodes: data,
+      },
+      revalidate: 60 * 60 * 8,
+    }
+}
+
+//SSR
+// export async function getServerSideProps() {
+//   const response = await fetch("http://localhost:3333/episodes");
+//   const data = await response.json();
+
+//   return { 
+//     props: {
+//       episodes: data,
+//     }
+//   }
+// }
+
+
